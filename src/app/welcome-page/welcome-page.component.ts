a16429661c8079028a038d36f3cad63cf806c72e
VadymMakohon <errorks7@gmail.com>
2024-06-21 10:49:50 +0200

    added a welcome page

---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-page.component.html',
+  styleUrl: './welcome-page.component.css'
+})
+export class WelcomePageComponent {
+
+}
