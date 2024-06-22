f737320dc5573f7eb2eb4bf9448c2559c6a4e824
VadymMakohon <errorks7@gmail.com>
2024-06-22 15:02:40 +0200

    updated welcome page

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-welcome-page',
-  standalone: true,
-  imports: [],
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.css'
+  styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent {
-
+export class WelcomePageComponent implements OnInit {
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void {
+  }
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
+    });
+  }
 }
